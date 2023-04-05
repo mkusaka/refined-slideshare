2f628ec6bc84b71357d72305310842c87aa27879
mkusaka <hinoshita1992@gmail.com>
2023-04-06 02:01:47 +0900

    improve

--- content.js
+++ content.js
@@ -10,5 +10,5 @@ function findEmbedURL() {
 
 const container = document.getElementById("new-player");
 if (container) {
-  container.innerHTML = `<iframe src="${findEmbedURL()}" width="1280" height="1010"frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px;" allowfullscreen>`;
+  container.innerHTML = `<iframe src="${findEmbedURL()}" width="1280" height="1010"frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%; max-height: 100%;" allowfullscreen>`;
 }
