188006dca2d2cf6f66058e84c4ebfbbbcfd6d692
mkusaka <hinoshita1992@gmail.com>
2023-04-27 00:18:40 +0900

    follow latest changes for slideshare

--- content.js
+++ content.js
@@ -1,16 +1,22 @@
-function findEmbedURL() {
-  let metaTags = document.getElementsByTagName("meta");
-  for (let tag of metaTags) {
-    if (tag.getAttribute("itemprop") === "embedURL") {
-      return tag.getAttribute("content");
+(() => {
+  // content.ts
+  function seekIframeInfo() {
+    const propsElement = document.getElementById("__NEXT_DATA__");
+    const propsJSON = JSON.parse(propsElement.innerHTML);
+    const iframeEmbed = propsJSON?.props?.pageProps?.slideshow?.iframeEmbed;
+    if (!iframeEmbed) {
+      console.log("cannot find valid iframeEmbed");
+      return null;
+    }
+    return iframeEmbed;
+  }
+  var container = document.getElementById("new-player");
+  if (container) {
+    const iframeInfo = seekIframeInfo();
+    if (iframeInfo) {
+      const { width: iframeWidth, height: iframeHeight, url } = iframeInfo;
+      const width = container.getBoundingClientRect().width;
+      container.innerHTML = `<iframe src="${url}" width="${width}" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%; aspect-ratio: ${iframeWidth} / ${iframeHeight};" allowfullscreen>`;
     }
   }
-  return null;
-}
-
-const container = document.getElementById("new-player");
-if (container) {
-  const width = container.getBoundingClientRect().width;
-  const height = container.getBoundingClientRect().height;
-  container.innerHTML = `<iframe src="${findEmbedURL()}" width="${width}" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%; aspect-ratio: ${width} / ${height};" allowfullscreen>`;
-}
+})();
---
+++ content.ts
@@ -0,0 +1,25 @@
+function seekIframeInfo() {
+  const propsElement = document.getElementById("__NEXT_DATA__");
+  const propsJSON  = JSON.parse(propsElement.innerHTML);
+  const iframeEmbed: {
+    height: number;
+    width: number;
+    url: string;
+  } | undefined = propsJSON?.props?.pageProps?.slideshow?.iframeEmbed;
+  if (!iframeEmbed) {
+    console.log("cannot find valid iframeEmbed");
+    return null;
+  }
+
+  return iframeEmbed;
+}
+
+const container = document.getElementById("new-player");
+if (container) {
+  const iframeInfo = seekIframeInfo();
+  if (iframeInfo) {
+    const { width: iframeWidth, height: iframeHeight, url } = iframeInfo;
+    const width = container.getBoundingClientRect().width;
+    container.innerHTML = `<iframe src="${url}" width="${width}" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%; aspect-ratio: ${iframeWidth} / ${iframeHeight};" allowfullscreen>`;
+  }
+}
