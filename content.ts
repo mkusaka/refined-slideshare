daca29f36568c01d32d4710b77a26af3c4b3238b
mkusaka <hinoshita1992@gmail.com>
2023-05-01 03:37:08 +0900

    hide modal

--- content.ts
+++ content.ts
@@ -1,11 +1,15 @@
+import * as Cookies from "js-cookie";
+
 function seekIframeInfo() {
   const propsElement = document.getElementById("__NEXT_DATA__");
-  const propsJSON  = JSON.parse(propsElement.innerHTML);
-  const iframeEmbed: {
-    height: number;
-    width: number;
-    url: string;
-  } | undefined = propsJSON?.props?.pageProps?.slideshow?.iframeEmbed;
+  const propsJSON = JSON.parse(propsElement.innerHTML);
+  const iframeEmbed:
+    | {
+        height: number;
+        width: number;
+        url: string;
+      }
+    | undefined = propsJSON?.props?.pageProps?.slideshow?.iframeEmbed;
   if (!iframeEmbed) {
     console.log("cannot find valid iframeEmbed");
     return null;
@@ -23,3 +27,5 @@ if (container) {
     container.innerHTML = `<iframe src="${url}" width="${width}" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%; aspect-ratio: ${width} / ${height};" allowfullscreen>`;
   }
 }
+
+Cookies.set("scribd_ad_exit_slideshow_page", true);
